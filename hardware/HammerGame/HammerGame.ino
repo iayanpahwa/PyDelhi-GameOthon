90693ecf7d96d8489da62f3adf4c86d4b870710b
Ayan Pahwa <iayanpahwa@gmail.com>
2017-11-11 14:39:58 +0530

    Stable shape

--- hardware/HammerGame/HammerGame.ino
+++ hardware/HammerGame/HammerGame.ino
@@ -1,24 +1,35 @@
 #define FSR A2
+#define startButton 3
+#define statusStart 13
+volatile byte state = LOW;
 
 int oldVal, newVal;
 
 void setup() { 
 
-Serial.begin(9600); //Initiate serial port
+  pinMode(statusStart, OUTPUT);
+  pinMode(startButton, INPUT_PULLUP);
+  Serial.begin(9600); //Initiate serial port
+  attachInterrupt(digitalPinToInterrupt(startButton), start, LOW);
+}
 
+void start(){
+
+  state = HIGH;
+  
 }
 
 void loop() {
-  
-int oldVal = analogRead(FSR); // Read value from sensor
-delay(1);
-int newVal = analogRead(FSR);
+
+  digitalWrite(statusStart, state);
+  int oldVal = analogRead(FSR); // Read value from sensor
+  delay(1);
+  int newVal = analogRead(FSR);
 
 if(newVal > oldVal){
-  
-Serial.println(newVal);
-delay(1);
-  
+  int newVal = analogRead(FSR);
+  Serial.println(newVal);  
+  delay(100);
 }
 
 }
